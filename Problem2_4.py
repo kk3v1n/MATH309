abd15848ff2660d504a9913d9959b75cf3f5019f
unknown <kevinkarmacharya@yahoo.com>
2021-11-15 00:12:24 -0800

    added exposure count per person and fixed infection rate in group

--- Problem2_4.py
+++ Problem2_4.py
@@ -20,7 +20,9 @@ class problem_two:
         for count in range(0, total_possibly_infected):
             population_chances[count].insert(0,count) #adding person's id to in front of each array
 
-        #print(population_chances) #print the array to seeing data for each person including thier id in index 0
+            population_chances[count].insert(5,0) #adding person's #of exposure at end of array
+
+        print(population_chances) #print the array to seeing data for each person including thier id in index 0
 
 
         infected_people_for_certain_if_close_contact = [] #creating a list of people who we know will be infected due to their "resistance" %
@@ -36,11 +38,20 @@ class problem_two:
         print("All these people will have a chance of being infected if in contact: ",infected_people_for_certain_if_close_contact)
 
         size_of_infected_people = len(infected_people_for_certain_if_close_contact)#getting size of array the infected people population
-        print("the max that could be infected due to their %: (no changes, no edits, just flat number) ",size_of_infected_people)
+        #print("the max that could be infected due to their %: (no changes, no edits, just flat number) ",size_of_infected_people)
 
 
         starting_with_person = random.choice(infected_people_for_certain_if_close_contact) # we will start with this person for this certain iteration
+        starting_with_person[5] +=1 #since we start with this person, they are now exposed (index 5 = #of exposure)
         print("We are starting with: ",starting_with_person)
+
+
+        total_population_chances = population_chances.copy() #copies the array to still have total array for every N in the population
+
+        population_chances.pop(starting_with_person[0]) #removes starter person out of overall array for iteration as they 
+        
+        #print(population_chances)
+        #print(total_population_chances)
 
         size_of_infected_people = size_of_infected_people - 1 #we can now exclude the person that is infected since we are initally working with them
 
@@ -55,14 +66,29 @@ class problem_two:
             number_of_people_exposed_rounded = round(people_exposed) #gives us # of people exposed
             print("count of people exposed by this person: ",number_of_people_exposed_rounded)
 
+            for count in range(number_of_people_exposed_rounded): #increasting exposure count in index5
+                exposed_person = random.choice(population_chances)
+                exposed_person[5] += 1
+
+            #print("ummm",population_chances) #to see how many times they got exposed by current person
+
+            who_can_be_infected = 0 # who actually can be infected due to resistance being low and exposure being high
+
+            for counter in range(0, total_possibly_infected_without_startingPerson):
+                if(population_chances[counter][5]>0 and population_chances[counter][1] < 0.6):
+                    who_can_be_infected +=1
+
+            print("max that can be infected due to resistance and exposure count (being a factor): ", who_can_be_infected)
+
+
             infection_rate_to_others_starting_person = starting_with_person[4]
             number_of_people_they_infected = infection_rate_to_others_starting_person*number_of_people_exposed_rounded #calculating number of people they infected
             number_of_people_they_infected_rounded = round(number_of_people_they_infected) #rounding to check #of exposures
 
-            if(number_of_people_they_infected_rounded <= size_of_infected_people):
+            if(number_of_people_they_infected_rounded <= who_can_be_infected):
                 print("count of people they did infect:", number_of_people_they_infected_rounded)
             else:
-                print("count of people they did infect 100%", size_of_infected_people)
+                print("count of people they did infect 100%", who_can_be_infected)
 
         else:
             print("person recovered and will not infect others :D")
