67df36f9ed6c380200964bc7345ee1047707038b
Jwong66 <jwong66@mail.sfsu.edu>
2021-11-18 02:41:09 -0800

    pausechamp

--- Problem3.py
+++ Problem3.py
@@ -1,46 +1,47 @@
 
 import numpy as np
 
-class problem_3():
-    
-    def __init__(self):
-        parsefile = self.getFile("forceAndAngle.csv")
-        
-        self.applied_forces = parsefile[0]
-        self.angle_of_applications = parsefile[1]
 
-    
-    def getFile(self, file_input):
-        
-        applied_forces = []
-        angle_of_applications = []
-        with open(file_input, "r") as data_file:
-            data_file.readline() #ignore header
-            for line in data_file:
-                force, angle = line.split(',')
-                applied_forces.append(float(force))
-                angle_of_applications.append(float(angle))
+def getFile(file_input):
+    applied_forces = []
+    angle_of_applications = []
+    with open(file_input, "r") as data_file:
+        data_file.readline() #ignore header
+        for line in data_file:
+            force, angle = line.split(',')
+            applied_forces.append(float(force))
+            angle_of_applications.append(float(angle))
 
-        return (applied_forces, angle_of_applications)
-    
-    def simpsons_rule(self, f, a, b, n):
-        h = (b - a) / n 
-        k = 0.0
-        x = a + h
-        for i in range (1, n//2 + 1):
-            k += 4*f(x)
-            x += 2*h
-            
-        x = a + 2*h
-        for i in range(1, n//2):
-            k += 2*f(x)
-            x += 2*h
-            
-        return (h/3) *( f(a)+ f(b) + k)
+    return (applied_forces, angle_of_applications)
 
-if __name__ == "__main__":
-    problem = problem_3()
-    #problem.simpsons_rule(10)
-    #print(len(problem.applied_forces))
-    #print(len(problem.angle_of_applications))
-    print(problem.simpsons_rule(lambda x: problem.applied_forces * np.cos(problem.angle_of_applications), 20, 50, 22 ))
+def calculation():
+    forces_array, angle_array = getFile("forceAndAngle.csv")
+    print("this is forces_array: ",forces_array)
+    print("this is angle_array: ",angle_array)
+
+    #a = input("Enter a bottom bound(a): ")
+    a = 20
+    #b = input("Enter a value for top bound(a): ")
+    b = 50
+    #n = input("Enter a even number: ")
+    n = 3 #you can change this to whatever
+    h = (b - a) / n
+    x = np.linspace(a, b, n)
+
+    print("this is x: ",x)
+
+    forces_array_count = len(forces_array)
+    angle_array_count = len(angle_array)
+    
+    #f = np.cos(x)
+    f = forces_array * np.cos(angle_array)
+
+    print("this is f: ", f)
+
+    I_simp = (h/3) * (f[0] + 2*sum(f[:n-2:2]) \
+        + 4*sum(f[1:n-1:2]) + f[n-1])
+
+    print(I_simp)
+
+print(calculation())
+
