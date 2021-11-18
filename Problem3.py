f1c0f50919b26aa5d13f47823007918ea267e4bf
Jwong66 <jwong66@mail.sfsu.edu>
2021-11-18 02:05:17 -0800

    More correct?

--- Problem3.py
+++ Problem3.py
@@ -8,9 +8,10 @@ class problem_3():
         
         self.applied_forces = parsefile[0]
         self.angle_of_applications = parsefile[1]
-        
-        
+
+    
     def getFile(self, file_input):
+        
         applied_forces = []
         angle_of_applications = []
         with open(file_input, "r") as data_file:
@@ -21,29 +22,25 @@ class problem_3():
                 angle_of_applications.append(float(angle))
 
         return (applied_forces, angle_of_applications)
-
-
-    def simpsons_rule(self, n): 
-        a = 20.0
-        b = 50.0 
-        h = b - a / n
-        x = np.linspace(a, b , n + 1)
-
-        #attempt 1
-        #f = lambda x : self.applied_forces * np.cos(self.angle_of_applications)
-        #y = f(x)
-        #simpson = h / 3 * (np.sum(y[0:-1:2] + 4*y[1::2] + y[2::2]))
-        
-        #attempt 2
-        f = self.applied_forces * np.cos(self.angle_of_applications) * x
-        simpson = (h/3) * (f[0] + 2*sum(f[n-2:2]) + 4*sum(f[1:n-1:2]) + f[n-1])
-        return simpson
-
     
+    def simpsons_rule(self, f, a, b, n):
+        h = (b - a) / n 
+        k = 0.0
+        x = a + h
+        for i in range (1, n//2 + 1):
+            k += 4*f(x)
+            x += 2*h
+            
+        x = a + 2*h
+        for i in range(1, n//2):
+            k += 2*f(x)
+            x += 2*h
+            
+        return (h/3) *( f(a)+ f(b) + k)
+
 if __name__ == "__main__":
     problem = problem_3()
     #problem.simpsons_rule(10)
-    print(len(problem.applied_forces))
-    print(len(problem.angle_of_applications))
-    print(problem.simpsons_rule(22))
-
+    #print(len(problem.applied_forces))
+    #print(len(problem.angle_of_applications))
+    print(problem.simpsons_rule(lambda x: problem.applied_forces * np.cos(problem.angle_of_applications), 20, 50, 22 ))
